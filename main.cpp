8921134dd060428a93841925bf6ddb053826903b
Erdem Yilmaz <erdemy885@gmail.com>
2024-07-24 21:59:13 -0400

    make still donut

--- main.cpp
+++ main.cpp
@@ -1,50 +1,42 @@
 #include <iostream>
 #include "ascii-display.h"
-#include <math.h>
+#include <cmath>
+
+using namespace std;
+
+const int height = 50;
+const int width = height*2;
+const int half = height/2;
+const int R1 = 20;
+const int R2 = 5;
 
 int main(){
-	const int height = 50;
-	const int width = height*2;
-	const int half = height/2;
+    Window win = Window(width, height);
 
-	float z = 0;
-
-	Window win = Window(width, height);
-	win.cls();
-	for(int i = 0; i < width; i++) {
-		win.drawc(i, half, '-');
-		win.drawc(height, i, '|');
-	}
-	win.drawc(height, half, '+');
-	float alpha = 0;
-	while(true){
-		float sin_alpha = sin(alpha);
-		float cos_alpha = cos(alpha);
-		for(float theta = 0; theta < 2 * M_PI; theta += 0.05){
-			float x = 20 + cos(theta) * 5;
-			float y2 = sin(theta) * 5;
-			for(float phi = 0; phi < 2 * M_PI; phi += 0.05){
-				float cos_phi = cos(phi);
-				float sin_phi = sin(phi);
-				float x2 = x*cos_phi + z*sin_phi;
-				float z2 = -x*sin_phi + z*cos_phi;
-				//float screen_x = x2 * inv_z * k;
-				//float screen_y = y2 * inv_z * k;
-
-				float x3 = x2*cos_alpha + z*sin_alpha;
-				float y3 = sin_alpha*(x2*sin_alpha - z2*cos_alpha) + y2*cos_alpha;
-				float z3 = cos_alpha*(z2*cos_alpha - x2*sin_alpha) + y2*sin_alpha;
-				float screen_x = 2*x3 + height;
-				float screen_y = -y3 + half;
-				win.draw(screen_x, screen_y, 12);
-			}
-		}
-		alpha += 0.001;
-		win.ref();
-		win.cls();
-	}
-	win.ref();
-	std::string input;
-	std::cin >> input;
-	return 0;
+    while(true){
+        //draw grid for reference
+        for(int i = 0; i < height; i++){
+            win.drawc(height, i, '|');
+            win.drawc(2*i, half, '-');
+            win.drawc(2*i + 1, half, '-');
+        }
+        win.drawc(height, half, '+');
+        //make circle with radius R2 centered at (R1, 0)
+        for(float theta = 0; theta < M_PI * 2; theta += 0.1){
+            float x = R1 + R2 * cos(theta);
+            float y = R2 * sin(theta);
+            float z = 0;
+            for(float phi = 0; phi < M_PI * 2; phi += 0.1){
+                float x2 = x * cos(phi) + z * sin(phi);
+                float y2 = y;
+                float z2 = z * cos(phi) - x * sin(phi);
+                int screen_x = round(2*x2) + height;
+                int screen_y = round(-y2) + half;
+                win.drawc(screen_x, screen_y, '*');
+            }
+        }
+        win.ref();
+        win.cls();
+    }
+    return 0;
 }
